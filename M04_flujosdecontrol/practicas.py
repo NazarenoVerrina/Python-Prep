1d0647cfe00d7ad63feeed81f98782ba40992318
NazarenoVerrina <159024764+NazarenoVerrina@users.noreply.github.com>
2025-10-22 22:48:01 -0300

    ejercicios terminados 03

---
+++ M04_flujosdecontrol/practicas.py
@@ -0,0 +1,28 @@
+a = 5
+b = 7
+
+if a < b:
+    print('a es menor que b')
+elif a > b:
+    print('a es mayor que b')
+else:
+    print('a y b son iguales')
+
+num = 5
+if num>10:
+    print("Soy mayor")
+elif num<10:
+    print("Soy menor")
+else:
+    print("Soy igual")
+
+
+num = 5
+if num>10:
+    print("Soy mayor")
+elif num<10:
+    print("Soy menor que diez y como estoy primero me voy a ejecutar")
+elif num<7:
+    print("Soy también menor")
+else:
+    print("Soy igual")
